008a1c97e55cd49bd0d9f2e13995e4edd2b26cf8
seanmyers0608 <seanmyers0608@gmail.com>
2011-12-05 19:38:58 +0000

    Added a player class, updating it as we speak.

---
+++ Player.h
@@ -0,0 +1,17 @@
+#ifndef PLAYER_H_
+#define PLAYER_H_
+
+class Player {
+public:
+	Player();
+	virtual ~Player();
+	bool hit(float x, float y, float z);
+	bool takeDamage(int amount);
+
+private:
+	int health;
+	Camera look;
+	Cube boundingBox;
+};
+
+#endif /* PLAYER_H_ */
