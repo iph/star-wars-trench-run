add94a30e719d862b9874470293098149df480a0
seanmyers0608@gmail.com <seanmyers0608@gmail.com@600a024b-c645-a5e9-483d-6196425671ff>
2011-12-10 06:26:42 +0000

    RAWR

--- trunk/Player.cpp
+++ trunk/Player.cpp
@@ -16,14 +16,19 @@ boundingBox(1, 1){
 
 	//TODO: Figure out the camera coords of starting game.
 	health = 100;
+	speed = 1;
 }
 bool Player::hit(float x, float y, float z){
 	// TODO: Check bounding box of ship with that of lasers/ outer objects
 	return false;
 }
+
 bool Player::takeDamage(int amount){
 	health -= amount;
 	return health > 0; //Returns false when game is over
+}
+void Player::moveForward(){
+	look.camLocation
 }
 Player::~Player() {
 	// TODO NOTHING :D
